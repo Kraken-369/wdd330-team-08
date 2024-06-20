3b0b2ddf1d579ff418050d47faf20d1866b055fc
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-20 16:00:18 -0700

    Reverted files back from the MAIN,

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getParams } from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
@@ -7,23 +6,3 @@ const dataSource = new ProductData("tents");
 const productId = getParams("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
-=======
-import { appendToCart } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-
-const dataSource = new ProductData("tents");
-
-function addProductToCart(product) {
-  appendToCart("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
->>>>>>> 49c0ecd577bb60c9d0a4f67ff16a749c0307d5ec
