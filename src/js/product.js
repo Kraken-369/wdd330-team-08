8fb20b9e18a0508ab94d97e05b0fed2f3955ca0b
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-19 21:12:22 -0700

    Weekly team commit!

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,23 @@
-import { appendToCart } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParams("product");
 
-function addProductToCart(product) {
-  appendToCart("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// function addProductToCart(product) {
+//   appendToCart("so-cart", product);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
