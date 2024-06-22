03be6ffa1416bc121dd567345b1450316e563fee
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-21 18:30:42 -0700

    Working on main.js, test is placeholder

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
 import ProductData from "./ProductData.mjs";
+import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
+const list = new ProductListing("tents", dataSource, "test");
