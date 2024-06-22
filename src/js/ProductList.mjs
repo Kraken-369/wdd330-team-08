dd6e2a9ad892e460d01eec5eb8b1c278896f14a1
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-21 20:15:38 -0700

    Finished template function and class initialization.

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,7 +1,15 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
 function createCard(product) {
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product}">
-        <img src="product_pages/" alt="Image of ">
+    return `
+<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+    </a>
+</li>
     `
 }
 export default class ProductListing {
@@ -12,5 +20,9 @@ export default class ProductListing {
     }
     async init() {
         const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+    renderList(list) {
+        renderListWithTemplate(createCard, this.listElement, list);
     }
 }
