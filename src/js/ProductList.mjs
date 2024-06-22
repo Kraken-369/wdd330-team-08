bf4319ffe1f89225de5ce6bd308386dc4706418b
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-21 18:30:07 -0700

    Working on creating template for products

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,8 +1,14 @@
+function createCard(product) {
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product}">
+        <img src="product_pages/" alt="Image of ">
+    `
+}
 export default class ProductListing {
-    constructor(category, dataSource, elementTarget) {
+    constructor(category, dataSource, listElement) {
         this.category = category;
         this.dataSource = dataSource;
-        this.elementTarget = elementTarget;
+        this.listElement = listElement;
     }
     async init() {
         const list = await this.dataSource.getData();
