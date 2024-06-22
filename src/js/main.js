bf8a6536a1c4e280920fc58818e6eee668a2db7a
AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-21 20:17:04 -0700

    Added grabbing data for listing and initializing to load dynamic product list.

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-const list = new ProductListing("tents", dataSource, "test");
+const getElement = document.querySelector(".product-list");
+const list = new ProductListing("tents", dataSource, getElement);
+
+list.init();
