3e0eddd475eed0e1ff91c9fd1c0161c5f27e5619
Nestor Otondo <oto23001@byui.edu>
2024-06-27 23:20:24 -0400

    Adding template loaders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
+import {loadHeaderFooter} from "./utils.mjs";
 
 const product = "tents";
 const dataSource = new ProductData(product);
 const listElement = document.querySelector(".product-list")
 const productList = new ProductListing(product, dataSource, listElement);
 
+loadHeaderFooter();
 productList.init();
