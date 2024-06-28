193e18574ffe0e5af6aff79ffbe3d1bb49b33d46
Austin B Campbell/AussieDev <131830841+AustinCBYUi@users.noreply.github.com>
2024-06-27 22:14:07 -0700

    Dynamically populates shopping cart.

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,32 @@
+import { renderListWithTemplate } from "./utils.mjs";
+//TODO
+var quantityVar = 1;
+
+const productInCartTemplate = (item) => `
+<li class="cart-card divider">
+    <a href="/product_pages/?product=${item.Id}" class="cart-card__image">
+        <img src="${item.Image}" alt="${item.Name}"/>
+    </a>
+    <a href="/product_pages/?product=${item.Id}">
+        <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card_quantity">Quantity: ${quantityVar}</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>
+`;
+
+export default class CartListing {
+    constructor(dataSource, listElement) {
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    renderCartList(list) {
+        renderListWithTemplate(productInCartTemplate, this.listElement, list);
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderCartList(list);
+    }
+}
