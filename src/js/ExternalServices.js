1ccc67f791c5e8144a3afb2f6a970cccd4b079db
Nestor Otondo <oto23001@byui.edu>
2024-07-10 18:34:00 -0400

    Adding checkout process

--- src/js/ExternalServices.js
+++ src/js/ExternalServices.js
@@ -1,21 +1,41 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-const convertToJson = res => {
+const convertToJson = async res => {
+  const data = await res.json();
+
   if (res.ok) {
-    return res.json();
+    return data;
   } else {
-    throw new Error("Bad Response");
+    throw { name: "servicesError", message: data };
   }
 }
 
-export const checkout = async payload => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  };
-	
-  return await fetch(baseURL + "checkout/", options).then(convertToJson);
+export default class ExternalServices {
+
+  getData = async category => {
+    const res = await fetch(baseURL + `products/search/${category}`);
+    const data = await convertToJson(res);
+
+    return data.Result;
+  }
+
+  findProductById = async id => {
+    const res = await fetch(baseURL + `product/${id}`);
+    const data = await convertToJson(res);
+
+    return data.Result;
+  }
+
+  checkout = async payload => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
+  }
+
 }
